6cf1bfbec989e9ecd069e6722c5bc40dcecbbc16
Li LEI <130347271+shannon-headmaster@users.noreply.github.com>
2023-08-07 10:05:17 +0800

    common

---
+++ Author/index.php
@@ -0,0 +1,34 @@
+<?php
+
+class index
+{
+    public function __construct()
+    {
+        var_dump('hello world');
+    }
+
+    public function index()
+    {
+        echo 'headmaster-it/index/index';
+    }
+
+    public function edit()
+    {
+        echo 'headmaster-it/index/edit';
+    }
+
+    public function delete()
+    {
+        echo 'headmaster-it/index/delete';
+    }
+
+    public function test()
+    {
+        echo 'headmaster-it/index/test';
+    }
+
+    public function test2()
+    {
+        echo 'headmaster-it/index/test2';
+    }
+}
